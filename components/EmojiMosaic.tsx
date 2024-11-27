86c52252c48ea55b3c916968afe8fb8e30b691db
Niranjanparte <parteniranjan7@gmsil.com>
2024-11-28 01:09:19 +0530

    Added html2canvas dependency and updated package-lock.json

---
+++ components/EmojiMosaic.tsx
@@ -0,0 +1,70 @@
+import React, { useRef } from "react";
+import html2canvas from "html2canvas";
+
+interface MosaicCell {
+  emoji: string; // Define the structure of each cell
+}
+
+interface EmojiMosaicProps {
+  mosaic: MosaicCell[]; // Array of objects with `emoji` property
+  gridSize?: number; // Optional grid size
+  backgroundColor?: string; // Optional background color
+}
+
+export default function EmojiMosaic({ mosaic, gridSize = 128, backgroundColor = "black" }: EmojiMosaicProps) {
+  const mosaicRef = useRef<HTMLDivElement>(null);
+  const [newBgColor, setNewBgColor] = React.useState(backgroundColor);
+
+  const handleDownload = async () => {
+    if (mosaicRef.current) {
+      const canvas = await html2canvas(mosaicRef.current, {
+        scale: 2,
+        useCORS: true,
+        logging: true,
+        width: mosaicRef.current.scrollWidth,
+        height: mosaicRef.current.scrollHeight,
+        backgroundColor: newBgColor,
+      });
+      const link = document.createElement("a");
+      link.href = canvas.toDataURL("image/png");
+      link.download = "mosaic.png";
+      link.click();
+    }
+  };
+
+  const style = {
+    display: "grid",
+    gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
+    gap: "1px",
+    padding: "10px",
+    borderRadius: "5px",
+    backgroundColor: newBgColor, // Set the background color for the mosaic
+  };
+
+  return (
+    <div className="flex m-4 flex-col items-center justify-center">
+      <div className="flex items-center">
+        <label htmlFor="color" className="text-2xl">Change background color</label>
+        <input
+          type="color"
+          value={newBgColor}
+          onChange={(e) => setNewBgColor(e.target.value)}
+          className="rounded-md m-4 w-16 cursor-pointer"
+        />
+      </div>
+      <button
+        className="bg-black px-4 py-3 rounded-md hover:text-gray-400 border-[1px] border-gray-400 m-4"
+        onClick={handleDownload}
+      >
+        Download Mosaic
+      </button>
+      <div ref={mosaicRef} style={style} className="h-auto w-fit">
+        {mosaic.map((cell, index) => (
+          <div key={index} style={{ fontSize: "4px" }}>
+            {cell.emoji}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
